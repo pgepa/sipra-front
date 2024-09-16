5ba9fa0f17fc9689eaa393611618b37dea46ad96
Anderson Pontes <andersonpinheiro.developer@gmail.com>
2024-09-16 14:05:25 -0300

    add Sidebar animada

---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,47 @@
+import { FileSearch, House, ChevronFirst, Scale, Mails  } from 'lucide-react';
+import { useState } from 'react';
+import { Link, useLocation } from 'react-router-dom';
+
+export function Sidebar() {
+    const location = useLocation(); 
+    const [open, setOpen] = useState(true); 
+
+    return (
+        <aside className={`bg-gray-200 p-3 relative transition-all duration-300 ${open ? 'w-64' : 'w-20'}`}>
+            <ChevronFirst 
+                className={`absolute cursor-pointer right-0 top-4 rounded-full w-7 border-3 text-gray-700 hover:text-gray-500 transition-transform ${open ? '' : 'rotate-180'}`}
+                onClick={() => setOpen(!open)} 
+            />
+            <nav className="space-y-2 mt-4 flex flex-col">
+                <Link
+                    to="/home"
+                    className={`flex items-center gap-2 p-2 rounded hover:bg-gray-300 ${location.pathname === "/home" ? "font-bold text-indigo-700" : "text-gray-500"}`}
+                >
+                    <House className="h-6 w-6" />
+                    {open && <span className='font-medium'>Home</span>}
+                </Link>
+                <Link
+                    to="/protesto"
+                    className={`flex items-center gap-2 p-2 rounded hover:bg-gray-300 ${location.pathname === "/protesto" ? "font-bold text-indigo-700" : "text-gray-500"}`}
+                >
+                    <FileSearch className="h-6 w-6" />
+                    {open && <span className='font-medium'>Protesto</span>}
+                </Link>
+                <Link
+                    to="/home"
+                    className={`flex items-center gap-2 p-2 rounded hover:bg-gray-300 ${location.pathname === "#" ? "font-bold text-indigo-700" : "text-gray-500"}`}
+                >
+                    <Mails className="h-6 w-6" />
+                    {open && <span className='font-medium'>Carta Cobrança</span>}
+                </Link>
+                <Link
+                    to="/home"
+                    className={`flex items-center gap-2 p-2 rounded hover:bg-gray-300 ${location.pathname === "#" ? "font-bold text-indigo-700" : "text-gray-500"}`}
+                >
+                    <Scale className="h-6 w-6" />
+                    {open && <span className='font-medium'>Ajuizamento</span>}
+                </Link>
+            </nav>
+        </aside>
+    );
+}
--- src/pages/_layouts/app.tsx
+++ src/pages/_layouts/app.tsx
@@ -1,9 +1,10 @@
+import { Sidebar } from '@/components/Sidebar';
 import { Header } from "@/components/ui/header";
-import { FileSearch, Handshake, UserRoundSearch, House } from 'lucide-react';
-import { Link, Outlet, useLocation } from "react-router-dom";
+
+import { Outlet } from "react-router-dom";
 
 export function AppLayout() {
-    const location = useLocation(); // Hook para obter a rota atual
+
 
     return (
         <div className="flex min-h-screen flex-col antialiased">
@@ -11,53 +12,17 @@ export function AppLayout() {
 
             <div className="flex flex-1">
 
-                <aside className="w-64 bg-gray-200 p-4">
-                    <nav className="space-y-2 flex flex-col">
-                    <Link
-                            to="/home"
-                            className={`flex items-center gap-2 p-2 rounded hover:bg-gray-300 ${location.pathname === "/home" ? "font-bold text-indigo-700" : "text-gray-500"
-                                }`}
-                        >
-                            <House className="h-6 w-6" />
-                            Home
-                        </Link>
-                        <Link
-                            to="/protesto"
-                            className={`flex items-center gap-2 p-2 rounded hover:bg-gray-300 ${location.pathname === "/protesto" ? "font-bold text-indigo-700" : "text-gray-500"
-                                }`}
-                        >
-                            <FileSearch className="h-6 w-6" />
-                            Protesto
-                        </Link>
+                <Sidebar />
 
-                        <Link
-                            to="/menu2"
-                            className={`flex items-center gap-2 p-2 rounded hover:bg-gray-300 ${location.pathname === "/menu2" ? "font-bold text-indigo-700" : "text-gray-500"
-                                }`}
-                        >
-                            <Handshake  className="h-6 w-6" />
-                            Menu 2
-                        </Link>
 
-                        <Link
-                            to="/menu3"
-                            className={`flex items-center gap-2 p-2 rounded hover:bg-gray-300 ${location.pathname === "/menu3" ? "font-bold text-indigo-700" : "text-gray-500"
-                                }`}
-                        >
-                            <UserRoundSearch  className="h-6 w-6" />
-                            Menu 3
-                        </Link>
-                    </nav>
-                </aside>
-
-                {/* Conteúdo principal */}
-                <main className="flex-1 flex flex-col p-8 pt-6 bg-gray-50">
-                    {/* O conteúdo será colocado aqui */}
+                
+                <main className="flex-1 flex flex-col p-8 pt-6 bg-gray-100">
+                    
                     <div className="flex-1">
                         <Outlet />
                     </div>
 
-                    {/* Footer sempre no final */}
+                    
                     <footer className="text-sm text-muted-foreground text-center mt-8">
                         Copyright &copy; PGE-PA {new Date().getFullYear()} | DTIGD - Todos
                         os direitos reservados.
