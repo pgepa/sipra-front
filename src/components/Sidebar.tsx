5b65796ac6c00290e5288b5427f0149a59adb3a1
Anderson Pontes <andersonpinheiro.developer@gmail.com>
2024-09-23 10:20:57 -0300

    add menu upload

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,10 +1,11 @@
-import { FileSearch, House, ChevronFirst, Scale, Mails, CloudUpload  } from 'lucide-react';
+import { FileSearch, House, ChevronFirst, Scale, Mails, CloudUpload, ChevronDown, TreePine } from 'lucide-react';
 import { useState } from 'react';
 import { Link, useLocation } from 'react-router-dom';
 
 export function Sidebar() {
     const location = useLocation(); 
     const [open, setOpen] = useState(true); 
+    const [uploadOpen, setUploadOpen] = useState(false);
 
     return (
         <aside className={`bg-gray-200 p-3 relative transition-all duration-300 ${open ? 'w-64' : 'w-20'}`}>
@@ -43,13 +44,71 @@ export function Sidebar() {
                     {open && <span className='font-medium'>Ajuizamento</span>}
                 </Link>
 
-                <Link
-                    to="/upload"
-                    className={`flex items-center gap-2 p-2 rounded hover:bg-gray-300 ${location.pathname === "/upload" ? "font-bold text-indigo-700" : "text-gray-500"}`}
-                >
-                    <CloudUpload className="h-6 w-6" />
-                    {open && <span className='font-medium'>Upload</span>}
-                </Link>
+                <div className="relative">
+                    <button
+                        onClick={() => setUploadOpen(!uploadOpen)}
+                        className={`flex items-center gap-2 p-2 w-full text-left rounded hover:bg-gray-300 ${location.pathname.startsWith("/upload") ? "font-bold text-indigo-700" : "text-gray-500"}`}
+                    >
+                        <CloudUpload className="h-6 w-6" />
+                        {open && <span className='font-medium'>Upload</span>}
+                        {open && <ChevronDown className={`ml-auto transition-transform ${uploadOpen ? "rotate-180" : ""}`} />}
+                    </button>
+
+                
+                    {uploadOpen && open && (
+                        <div className="ml-6 space-y-1">
+                            <Link
+                                to="/upload/docs"
+                                className={`block p-2 rounded hover:bg-gray-300 ${location.pathname === "/upload/docs" ? "font-bold text-indigo-700" : "text-gray-500"}`}
+                            >
+                                Semas
+                            </Link>
+                            <Link
+                                to="/upload/images"
+                                className={`block p-2 rounded hover:bg-gray-300 ${location.pathname === "/upload/images" ? "font-bold text-indigo-700" : "text-gray-500"}`}
+                            >
+                                Adepara
+                            </Link>
+                            <Link
+                                to="/upload/videos"
+                                className={`block p-2 rounded hover:bg-gray-300 ${location.pathname === "/upload/videos" ? "font-bold text-indigo-700" : "text-gray-500"}`}
+                            >
+                                Jucepa PJ
+                            </Link>
+
+                            <Link
+                                to="/upload/videos"
+                                className={`block p-2 rounded hover:bg-gray-300 ${location.pathname === "/upload/videos" ? "font-bold text-indigo-700" : "text-gray-500"}`}
+                            >
+                                Jucepa vínculo
+                            </Link>
+                            <Link
+                                to="/upload/videos"
+                                className={`block p-2 rounded hover:bg-gray-300 ${location.pathname === "/upload/videos" ? "font-bold text-indigo-700" : "text-gray-500"}`}
+                            >
+                                Detran - renach
+                            </Link>
+                            <Link
+                                to="/upload/videos"
+                                className={`block p-2 rounded hover:bg-gray-300 ${location.pathname === "/upload/videos" ? "font-bold text-indigo-700" : "text-gray-500"}`}
+                            >
+                                Detran - sidet
+                            </Link>
+                            <Link
+                                to="/upload/videos"
+                                className={`block p-2 rounded hover:bg-gray-300 ${location.pathname === "/upload/videos" ? "font-bold text-indigo-700" : "text-gray-500"}`}
+                            >
+                                Detran carga veículo
+                            </Link>
+                            <Link
+                                to="/upload/videos"
+                                className={`block p-2 rounded hover:bg-gray-300 ${location.pathname === "/upload/videos" ? "font-bold text-indigo-700" : "text-gray-500"}`}
+                            >
+                                Detran carga modelo
+                            </Link>
+                        </div>
+                    )}
+                </div>
             </nav>
         </aside>
     );
