0445df591b85ca55b699a115cdc68e86b6b5c697
Anderson Pontes <andersonpinheiro.developer@gmail.com>
2024-12-17 10:43:29 -0300

    ajuste na exibição status database

--- src/pages/app/UltimaAtualizacaoDatabase.tsx
+++ src/pages/app/UltimaAtualizacaoDatabase.tsx
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import { api } from '@/lib/axios';
-import { Badge } from "@/components/ui/badge"
+import { Badge } from "@/components/ui/badge";
 import { Helmet } from 'react-helmet-async';
+import { Bot, Cog } from 'lucide-react'; // Importação dos ícones
 
 interface UserData {
     tabela: string;
@@ -21,6 +22,14 @@ export const UltimaAtualizacaoDatabase: React.FC = () => {
     const [data, setData] = useState<UserData[]>([]);
     const [loading, setLoading] = useState(true); // Estado para carregamento
     const [error, setError] = useState<string | null>(null); // Estado para erro
+
+    const tabelasManuais = [
+        'jucepapj',
+        'jucepavinculo',
+        'detrancargaveiculo',
+        'detranmarcamodelo',
+        'adepara'
+    ];
 
     useEffect(() => {
         const token = localStorage.getItem('token');
@@ -43,49 +52,61 @@ export const UltimaAtualizacaoDatabase: React.FC = () => {
             });
     }, []);
 
-
     if (loading) {
         return <div className="text-center">Carregando...</div>;
     }
-
 
     if (error) {
         return <div className="text-center text-red-500">{error}</div>;
     }
 
     return (
-
         <>
-
-            <Helmet title="Última Atualização" />
-
-
-            <div className='flex flex-col gap-4'>
-
-                <h1 className='text-2xl font-bold text-slate-700'>Última Atualização - Database</h1>
+            <Helmet title="Status Database" />
+            <div className="flex flex-col gap-4">
+                <h1 className="text-2xl font-bold text-slate-700 text-center">Status Database</h1>
 
                 <div className="w-full max-w-4xl mx-auto p-6">
-
                     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-4">
-                        {data.map((dados, index) => (
-                            <div key={index} className="flex justify-between items-center bg-white p-4 rounded-lg shadow-md hover:shadow-lg transition-shadow border-b border-gray-200">
-                                <div className="flex flex-col gap-2">
+                        {data.map((dados, index) => {
+                            const status = tabelasManuais.includes(dados.tabela) ? "manual" : "automática";
 
-                                    <span className="text-lg font-semibold flex gap-2 text-gray-800">
-                                        
-                                        <Badge variant="default" className='text-base'>Tabela: {dados.tabela}</Badge>
+                            return (
+                                <div
+                                    key={index}
+                                    className="flex justify-between items-center bg-white p-4 rounded-lg shadow-md hover:shadow-lg transition-shadow border-b border-gray-200"
+                                >
+                                    <div className="flex flex-col gap-2">
+                                        <span className="text-lg font-semibold flex gap-2 text-gray-800">
+                                            <Badge variant="default" className="text-base">
+                                                Tabela: {dados.tabela}
+                                            </Badge>
+                                        </span>
+                                        <span className="text-muted-foreground">
+                                            Última Atualização: {formatDate(dados.ultima_atualizacao)}
+                                        </span>
+                                    </div>
 
-                                    </span>
-                                    <span className="text-muted-foreground">Última Atualização: {formatDate(dados.ultima_atualizacao)}</span>
+                                    {/* Exibição do Status alinhado à direita com ícone */}
+                                    <div className="flex items-center gap-2 text-lg font-semibold text-gray-800">
+                                        {status === "manual" ? (
+                                            <>
+                                                <Cog className="text-rose-600" size={20} /> 
+                                                <span className="text-rose-600">Manual</span>
+                                            </>
+                                        ) : (
+                                            <>
+                                                <Bot className="text-violet-500" size={20} />
+                                                <span className="text-violet-500">Automática</span>
+                                            </>
+                                        )}
+                                    </div>
                                 </div>
-                            </div>
-                        ))}
+                            );
+                        })}
                     </div>
                 </div>
-
             </div>
-
         </>
-
     );
 };
