e58267959305a1d14962b20444c2ba641ef56655
Anderson Pontes <andersonpinheiro.developer@gmail.com>
2025-01-30 13:29:40 -0300

    ajuste header

--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -7,7 +7,7 @@ export function Header() {
 
     return (
         <div className={`border-b`}>
-            <div className="flex h-16 items-center gap-6 justify-start px-6 bg-gradient-to-r from-indigo-500 shadow-xl">
+            <div className="flex h-16 items-center gap-6 justify-start px-6 bg-gradient-to-r from-indigo-500 to-indigo-100 shadow-xl">
                 <div className="flex items-center gap-3 text-white">
                 <img className="h-12 w-12 text-white" src={logo} alt="Logo" />
                 <Separator orientation="vertical" className="h-6 hidden lg:block" />
