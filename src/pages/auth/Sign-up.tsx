077a2ba02cad0efea3deb9e85fd6be303d90cd0b
Anderson Pontes <andersonpinheiro.developer@gmail.com>
2025-04-03 10:10:17 -0300

    alteração novo usuário

--- src/pages/auth/Sign-up.tsx
+++ src/pages/auth/Sign-up.tsx
@@ -124,7 +124,7 @@ export function SignUp() {
                                             <SelectItem value="Chefia">Coordenação</SelectItem>
                                             <SelectItem value="Procurador">Procurador</SelectItem>
                                             <SelectItem value="Assessor">Assessor</SelectItem>
-                                            <SelectItem value="Estagiario">Estagiário</SelectItem>
+                                            <SelectItem value="Estagiario">Externo</SelectItem>
                                         </SelectContent>
                                     </Select>
                                 )}
