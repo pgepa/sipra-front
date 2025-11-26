50f202283d3186ce564cb831513ccd8abcaa0254
Anderson Pontes <andersonpinheiro.developer@gmail.com>
2025-11-26 10:44:53 -0300

    refactor: painel gestão pda

--- src/pages/app/DashboradAcompanhamentoPda.tsx
+++ src/pages/app/DashboradAcompanhamentoPda.tsx
@@ -3,16 +3,16 @@ import { Helmet } from 'react-helmet-async';
 export function AcompanhamentoPda() {
     return (
         <>
-            <Helmet title="Acompanhamento PDA" />
+            <Helmet title="Gestão de Devedores" />
             <div className='h-screen flex flex-col -mt-4'>
                 
                 <div className="flex flex-1">
                    
                     <iframe 
-                        title="Consulta Devedor - CNPJ" 
+                        title="Gestão de Devedores" 
                         width="100%" 
                         height="100%" 
-                        src="https://app.powerbi.com/view?r=eyJrIjoiMWQ5NTE4NjMtZjkyNy00YWUzLTlhY2UtM2NkZDBmZjJhZWNjIiwidCI6ImIwYmUwNjYzLTkxNWUtNDczZS1hNTU2LWExNmVmOWRmZTQxNCJ9" 
+                        src="https://app.powerbi.com/view?r=eyJrIjoiNmIxNGIzNWUtNjU5Yi00MGY3LWJjZGUtYjdhN2RkNmRiN2QzIiwidCI6ImIwYmUwNjYzLTkxNWUtNDczZS1hNTU2LWExNmVmOWRmZTQxNCJ9" 
                         allowFullScreen={true}
                         style={{ border: 'none' }}                        
                         >
